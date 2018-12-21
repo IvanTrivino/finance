923ddf0515e09e6ccc5773b4ac01e466c0cdcdbb
Ivan Camilo Triviño López <ivancamilo.trivi@icloud.com>
2018-12-21 13:21:55 -0500

    Initial Commit

---
+++ Finanzas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Finanzas
+//
+//  Created by Ivan Camilo Triviño López on 12/21/18.
+//  Copyright © 2018 Ivan Camilo Triviño López. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
